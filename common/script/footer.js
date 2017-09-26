97cce67414e9b7535ad4e03ce9cea1c27c0d8c42
yanjunfeng <yanjunfeng2011@live.cn>
2017-09-26 11:42:17 +0800

    优化：登录页面。

---
+++ common/script/footer.js
@@ -0,0 +1,20 @@
+/*
+ *  Javascript Footer
+ *  author phoenix-varus
+ */
+document.writeln("<div class='container i-footer'>");
+document.writeln("<span class='fa fa-github i-fa'></span>");
+document.writeln("<span class='fa fa-stack-overflow i-fa'></span>");
+document.writeln("<span class='fa fa-linkedin-square i-fa'></span>");
+document.writeln("<span class='fa fa-qq i-fa'></span>");
+document.writeln("<span class='fa fa-angellist i-fa'></span>");
+document.writeln("<span class='i-hone-hone-clock'><script charset='UTF-8' src='http://chabudai.sakura.ne.jp/blogparts/honehoneclock/honehone_clock_tr.js'></script></span>");
+document.writeln("<div class='index-copyright'>Copyright © 2017 iSkyCode</div>");
+document.writeln("</div>");
+document.writeln("<style type='text/css'>");
+document.writeln(".i-footer {margin-top: 30px;max-height: 102px;}");
+document.writeln(".i-fa {font-size: 36px;margin: 3px;color: #000000;}");
+document.writeln(".i-hone-hone-clock {float: right;margin-top: -22px;}");
+document.writeln(".index-copyright {margin-top: 20px;margin-bottom: 20px;text-align: center;}");
+document.writeln("</style>");
+
---
+++ common/script/taglibs.js
@@ -0,0 +1,21 @@
+/*
+ *  Javascript Header
+ *  author phoenix-varus
+ */
+<!--Icon-->
+document.writeln("<link rel='shortcut icon' href='../../favicon.ico'>");
+<!--Bootstrap-->
+document.writeln("<link rel='stylesheet' href='../../css/bootstrap-theme.min.css'/>");
+document.writeln("<link rel='stylesheet' href='../../css/bootstrap.min.css'/>");
+<!--Font Awesome-->
+document.writeln("<link rel='stylesheet' href='https://cdn.bootcss.com/font-awesome/4.7.0/css/font-awesome.css'>");
+<!--JQuery-->
+document.writeln("<script type='text/javascript' src='../../script/jquery-3.2.1.min.js'></script>");
+<!--Bootstrap-->
+document.writeln("<script type='text/javascript' src='../../script/bootstrap.min.js'></script>");
+
+document.writeln("<script type='text/javascript' src='../../script/random-dot.js'></script>");
+<!--Font-->
+document.writeln("<style type='text/css'>");
+document.writeln("@font-face {font-family: 'dnmbxsxd'; src: url('../../fonts/duanningmaobixingshuxiuding.ttf');}");
+document.writeln("</style>");
